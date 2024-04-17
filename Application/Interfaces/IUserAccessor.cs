546211d003b8972c118a77746f029a24b742f524
Drunkenseagull <john.cleyle@gmail.com>
2024-04-17 08:27:44 -0600

    Section 14 Checkpoint

---
+++ Application/Interfaces/IUserAccessor.cs
@@ -0,0 +1,9 @@
+﻿// interfaces in this folder are used to interact with other projects without creating dependencies
+
+namespace Application.Interfaces
+{
+	public interface IUserAccessor
+	{
+		string GetUsername();
+	}
+}
