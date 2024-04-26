1f640bc82c5c08a05dc7d2275b841c2d4e22491e
Drunkenseagull <john.cleyle@gmail.com>
2024-04-26 17:12:09 -0600

    Section 17 end - Create client side image handleing:   - image upload   - image cropping   - image delete   - image select main

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -9,19 +9,19 @@ export default observer(function NavBar() {
     <Menu inverted fixed='top'>
       <Container>
         <Menu.Item as={NavLink} to='/' header>
-          <img src="assets/logo.png" alt="logo" style={{marginRight: 10}} />
+          <img src="assets/logo.png" alt="logo" style={{ marginRight: 10 }} />
           Reactivities
         </Menu.Item>
         <Menu.Item as={NavLink} to='/activities' name="Activites" />
         <Menu.Item>
-          <Button as={NavLink} to='/createActivity' positive content ="Create Activity"/>
-          <Button as={NavLink} to='/errors' positive content ="Errors"/>
+          <Button as={NavLink} to='/createActivity' positive content="Create Activity" />
+          <Button as={NavLink} to='/errors' positive content="Errors" />
         </Menu.Item>
         <Menu.Item position='right'>
           <Image avatar spaced='right' src={user?.image || '/assets/user.png'} />
           <Dropdown pointing='top left' text={user?.displayName}>
             <Dropdown.Menu>
-              <Dropdown.Item as={Link} to={`/profile/${user?.username}`} text='My Profile' icon='user' />
+              <Dropdown.Item as={Link} to={`/profiles/${user?.username}`} text='My Profile' icon='user' />
               <Dropdown.Item onClick={logout} text='Logout' icon='power' />
             </Dropdown.Menu>
           </Dropdown>
--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -45,4 +45,12 @@ export default class UserStore {
       console.log(error);
     }
   }
+
+  setImage = (image: string) => {
+    if (this.user) this.user.image = image;
+  }
+
+  setUserPhoto = (url: string) => {
+    if (this.user) this.user.image = url;
+  }
 }
