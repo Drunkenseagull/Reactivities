a27766437189dcc23853060c5e919653b8b655e0
Drunkenseagull <john.cleyle@gmail.com>
2024-05-30 08:31:27 -0600

    checkpoint

---
+++ API/Controllers/FollowController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Followers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+	public class FollowController : BaseAPIController
+	{
+		[HttpPost("{username}")]
+		public async Task<IActionResult> Follow(string username)
+		{
+			return HandleResult(await Mediator.Send(new FollowToggle.Command
+			{ TargetUsername = username }));
+		}
+
+		[HttpGet("{username}")]
+		public async Task<IActionResult> GetFollowings(string username, string predicate) // username coem from route, predicate comes from query string
+		{
+			return HandleResult(await Mediator.Send(new List.Query { Username = username, Predicate = predicate }));
+		}
+	}
+}
